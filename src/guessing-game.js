7da4205fd4d061216d826d6213b8cd83834ef50d
Nastassia Arkharava <nastassiaarkharava@gmail.com>
2023-01-31 20:55:17 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+			this.minValue = min;
+			this.maxValue = max;
     }
 
     guess() {
-
+			return this.guessNumber = Math.round((this.maxValue + this.minValue) / 2);
     }
 
     lower() {
-
+			return this.maxValue = this.guessNumber;
     }
 
     greater() {
-
+			return this.minValue = this.guessNumber;
     }
 }
 
